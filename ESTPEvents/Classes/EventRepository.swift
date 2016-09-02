381b733243c06c31b70ca20496fcd5c6f027f5c3
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-02 19:28:57 +0200

    Create EventRepository

---
+++ ESTPEvents/Classes/EventRepository.swift
@@ -0,0 +1,12 @@
+//
+//  EventRepository.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 02/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+protocol EventRepository {
+}
