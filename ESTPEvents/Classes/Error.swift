9a2b217d9d5612bc0502fc17b4c0286fb86278af
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-03 15:05:52 +0200

    Create Error

---
+++ ESTPEvents/Classes/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 02/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+enum ApplicationError {
+    case generic
+}
