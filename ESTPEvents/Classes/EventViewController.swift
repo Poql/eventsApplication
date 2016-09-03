010ac6eec76379c014bef1cf13300e277ddddf6a
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-03 15:31:15 +0200

    Create EventViewController

---
+++ ESTPEvents/Classes/EventViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EventViewController.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 03/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: SharedViewController {
+}
