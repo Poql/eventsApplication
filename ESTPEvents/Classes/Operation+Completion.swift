7f60443d97c31c4c65a3a970e67a6f2e0101d47f
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-03 15:00:01 +0200

    Create Operation+Completion

---
+++ ESTPEvents/Classes/Operation+Completion.swift
@@ -0,0 +1,20 @@
+//
+//  Operation+Error.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 02/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import Operations
+
+extension Operation {
+    func addCompletionBlockOnMainQueue(block: () -> Void) {
+        addCompletionBlock {
+            dispatch_async(dispatch_get_main_queue()) {
+                block()
+            }
+        }
+    }
+}
