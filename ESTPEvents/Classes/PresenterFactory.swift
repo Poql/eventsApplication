d2563b73a092c5b3d7ca8a26ce7c7533e8268be6
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-03 15:22:01 +0200

    Create PresenterFactory

---
+++ ESTPEvents/Classes/PresenterFactory.swift
@@ -0,0 +1,12 @@
+//
+//  PresenterFactory.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 03/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+protocol PresenterFactory {
+}
