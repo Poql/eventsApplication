e2589fd9efd2617a721fae31edac31683ed9a84e
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-03 14:54:07 +0200

    Create SharedViewController

---
+++ ESTPEvents/Classes/SharedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SharedViewController.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 02/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+class SharedViewController: UIViewController {
+}
+
