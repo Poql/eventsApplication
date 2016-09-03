a90263ed0ba167e5c6fac4536767697df12c65a9
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-03 17:01:45 +0200

    Add presenter factory in AppDelegate

--- ESTPEvents/Classes/AppDelegate.swift
+++ ESTPEvents/Classes/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private let presenterFactory = PresenterFactoryImplementation()
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
