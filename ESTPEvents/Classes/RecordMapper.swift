28fe5c842ab43bec7560098b05f60db1225a1149
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-06 15:24:53 +0200

    Create RecordMapper

---
+++ ESTPEvents/Classes/RecordMapper.swift
@@ -0,0 +1,14 @@
+//
+//  RecordMapper.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 04/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+import BNRCoreDataStack
+
+struct RecordMapper {
+}
