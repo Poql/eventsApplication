305cd7f3b9e0383d97602557ac4ce350c6de5c92
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-06 15:50:47 +0200

    Add fetched results controller in EventPresenterImplementation

--- ESTPEvents/Classes/EventPresenterImplementation.swift
+++ ESTPEvents/Classes/EventPresenterImplementation.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Operations
+import BNRCoreDataStack
 
 class EventPresenterImplementation<R: EventRepository, PR: PersistencyRepository>: EventPresenter {
     private let eventRepository: R
@@ -17,14 +18,34 @@ class EventPresenterImplementation<R: EventRepository, PR: PersistencyRepository
     private let operationQueue = OperationQueue()
 
     private(set) var events: [Event] = []
+    
+    private var resultsController: FetchedResultsController<PersistentEvent>?
 
     weak var client: EventPresenterClient?
     
     init(repository: R, persistencyRepository: PR) {
         self.eventRepository = repository
         self.persistencyRepository = persistencyRepository
+        loadResultsController()
+    }
+
+    // MARK: - Private
+
+    private func loadResultsController() {
+        let operation = ReturnCoreDataStackOperation()
+        operation.addCompletionBlockOnMainQueue {
+            guard let mainContext = operation.stack?.mainQueueContext else { return }
+            self.setResultsController(with: mainContext)
+        }
+        operationQueue.addOperation(operation)
     }
     
+    private func setResultsController(with context: NSManagedObjectContext) {
+        let request = NSFetchRequest(entity: PersistentEvent.self)
+        request.sortDescriptors = [NSSortDescriptor(key: "eventDescription", ascending: true)]
+        resultsController = FetchedResultsController<PersistentEvent>(fetchRequest: request, managedObjectContext: context)
+    }
+
     // MARK: - EventPresenter
 
     func queryAllEvents() {
