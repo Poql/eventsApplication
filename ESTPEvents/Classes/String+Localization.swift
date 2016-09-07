b0a0c1db28a6e26f35051eb94cde01b04fabd667
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-07 11:10:42 +0200

    Create String + Localization

---
+++ ESTPEvents/Classes/String+Localization.swift
@@ -0,0 +1,15 @@
+//
+//  String+Localization.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 07/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    init(key: String) {
+        self = NSLocalizedString(key, comment: "")
+    }
+}
