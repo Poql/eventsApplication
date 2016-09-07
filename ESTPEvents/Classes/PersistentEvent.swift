871319ead25b895a6cd2c5a2f103ba8f5e906e73
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-07 09:55:47 +0200

    Update PersistentEvent

--- ESTPEvents/Classes/PersistentEvent.swift
+++ ESTPEvents/Classes/PersistentEvent.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 import CoreData
 import BNRCoreDataStack
 
@@ -14,6 +15,37 @@ class PersistentEvent: PersistentRecord {
     override class var entityName: String {
         return "PersistentEvent"
     }
+    
+    var location: CLLocation? {
+        set {
+            locationLongitude = newValue?.coordinate.longitude ?? 200
+            locationLatitude = newValue?.coordinate.latitude ?? 200
+        }
+        get {
+            guard locationLongitude <= 180 && locationLatitude <= 180 else { return nil }
+            return CLLocation(latitude: locationLatitude, longitude: locationLongitude)
+        }
+    }
+    
+    @NSManaged var type: String
+    
+    @NSManaged var title: String
 
     @NSManaged var eventDescription: String
+    
+    @NSManaged var color: String
+    
+    @NSManaged var creator: String
+    
+    @NSManaged var eventDate: NSDate
+    
+    @NSManaged var link: String
+    
+    @NSManaged var locationLatitude: Double
+    
+    @NSManaged var locationLongitude: Double
+    
+    @NSManaged var notify: Bool
+
+    @NSManaged var cancelled: Bool
 }
