525346b5e9aad4bd561186267455da0c409448b8
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-07 14:50:42 +0200

    Create ApplicationPresenter

---
+++ ESTPEvents/Classes/ApplicationPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationPresenter.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 07/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+protocol ApplicationPresenter {
+    func deleteEvents(beforeDate date: NSDate)
+}
