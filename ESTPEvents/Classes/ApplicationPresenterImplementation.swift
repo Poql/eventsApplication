64fd07876ceac2e3112e283d898179333f115662
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-08 18:18:20 +0200

    Add notification handling in ApplicationPresenter

--- ESTPEvents/Classes/ApplicationPresenterImplementation.swift
+++ ESTPEvents/Classes/ApplicationPresenterImplementation.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Operations
+import CloudKit
 
 class ApplicationPresenterImplementation<PR: PersistencyRepository, SR: SubscriptionRepository>: ApplicationPresenter {
     let operationQueue = OperationQueue()
@@ -28,5 +29,28 @@ class ApplicationPresenterImplementation<PR: PersistencyRepository, SR: Subscrip
         let eventOperation = subscriptionRepository.ensureEventsSubscriptionOperation()
         let userEventOperation = subscriptionRepository.ensureNotifyUserOnEventCreationOperation()
         operationQueue.addOperations(eventOperation, userEventOperation)
+    }
+    
+    func handleRemoteNotification(withUserInfo userInfo: [NSObject : AnyObject], completionHandler: (UIBackgroundFetchResult) -> Void) {
+        guard let userInfo = userInfo as? [String : NSObject] else { return }
+        let notification = CKQueryNotification(fromRemoteNotificationDictionary: userInfo)
+        guard let recordID = notification.recordID else { return }
+        let operation = subscriptionRepository.fetchRecordOperation(recordName: recordID.recordName)
+        let persistEventOperation = persistencyRepository.persistEventsOperation()
+        (persistEventOperation as Operation).addDependency(operation)
+        operation.addCompletionBlockOnMainQueue {
+            if let event = operation.resultingRecord as? Event {
+                persistEventOperation.events = [event]
+            }
+        }
+        let group = GroupOperation(operations: operation, persistEventOperation)
+        group.addCompletionBlockOnMainQueue {
+            if !group.errors.isEmpty {
+                completionHandler(.Failed)
+            } else {
+                completionHandler(.NewData)
+            }
+        }
+        operationQueue.addOperations(operation, persistEventOperation)
     }
 }
