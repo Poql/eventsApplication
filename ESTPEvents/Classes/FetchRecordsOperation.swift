c896a5c429238b8aee56ff8513bb8c1f8bc940b2
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-08 15:42:15 +0200

    Create FetchRecordsOperation

---
+++ ESTPEvents/Classes/FetchRecordsOperation.swift
@@ -0,0 +1,38 @@
+//
+//  FetchRecordsOperation.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 08/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Operations
+import CloudKit
+
+class FetchRecordsOperation: CloudOperation {
+    private let fetchRecordsOperation: CKFetchRecordsOperation
+    
+    var recordIDs: [CKRecordID]? {
+        set {
+            fetchRecordsOperation.recordIDs = recordIDs
+        }
+        get {
+            return fetchRecordsOperation.recordIDs
+        }
+    }
+    
+    private(set) var resultingRecords: [CKRecordID : CKRecord]?
+    
+    init(recordIDs: [CKRecordID], database: CKDatabase = CKContainer.defaultContainer().publicCloudDatabase) {
+        self.fetchRecordsOperation = CKFetchRecordsOperation(recordIDs: recordIDs)
+        super.init(database: database)
+    }
+    
+    override func execute() {
+        fetchRecordsOperation.fetchRecordsCompletionBlock = { recordsDictionary, error in
+            self.resultingRecords = recordsDictionary
+            self.finish(error)
+        }
+        database.addOperation(fetchRecordsOperation)
+    }
+}
