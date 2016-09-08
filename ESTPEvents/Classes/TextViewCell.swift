8c45bb2fa3ae5d9092a6a83d1a5c1abdc12bdf95
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-08 21:48:41 +0200

    Create TextViewCell

---
+++ ESTPEvents/Classes/TextViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  TextViewCell.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 08/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+class TextViewCell: UITableViewCell, UITextViewDelegate {
+
+    @IBOutlet var textView: UITextView!
+    
+    var placeholder = ""
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupViews()
+    }
+
+    // MARK: - Configuration
+
+    func configure(placeholder placeholder: String, text: String?) {
+        self.placeholder = placeholder
+        textView.text = text ?? placeholder
+        textView.textColor = text == nil ? .lightGrayColor() : .blackColor()
+    }
+
+    // MARK: - Private
+
+    private func setupViews() {
+        textView.delegate = self
+    }
+
+    // MARK: - UITextViewDelegate
+
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textView.text == placeholder && textView.textColor == .lightGrayColor() {
+            textView.text = nil
+            textView.textColor = .blackColor()
+        }
+    }
+
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = placeholder
+            textView.textColor = .lightGrayColor()
+        }
+    }
+}
