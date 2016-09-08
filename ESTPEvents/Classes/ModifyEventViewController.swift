f96fc59295a6a824a4c25aab7b013c1d376538c2
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-08 20:27:18 +0200

    Create ModifyEventViewController

---
+++ ESTPEvents/Classes/ModifyEventViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ModifyEventViewController.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 08/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+class ModifyEventViewController: SharedViewController {
+}
