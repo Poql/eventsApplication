660906dfcfccc5eebc4ba9c6c3990d28b351918f
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-10 14:15:15 +0200

    Add tableview in ModifyEventViewController

--- ESTPEvents/Classes/ModifyEventViewController.swift
+++ ESTPEvents/Classes/ModifyEventViewController.swift
@@ -8,5 +8,49 @@
 
 import UIKit
 
-class ModifyEventViewController: SharedViewController {
+class ModifyEventViewController: SharedViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+    var event: Event?
+    // MARK: - UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupController()
+    }
+
+    // MARK: - Private
+
+    private func setupController() {
+        title = String(key: "modify_event_title")
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return ModifyEventSection.count
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = ModifyEventSection(rawValue: section) else { return 0 }
+        switch section {
+        case .preview:
+            return ModifyEventPreviewRow.count
+        case .creator:
+            return ModifyEventCreatorRow.count
+        case .detail:
+            return ModifyEventDetailRow.count
+        case .adjunct:
+            return ModifyEventAdjunctRow.count
+        }
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
