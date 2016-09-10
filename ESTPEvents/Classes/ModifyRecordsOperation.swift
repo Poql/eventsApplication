6dca1b3444fa0637fecad4ffbcc55ad6ad24f708
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-09-10 20:07:58 +0200

    Create ModifyRecordsOperation

---
+++ ESTPEvents/Classes/ModifyRecordsOperation.swift
@@ -0,0 +1,46 @@
+//
+//  ModifyRecordsOperation.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 10/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+import Operations
+
+class ModifyRecordsOperation: CloudOperation {
+    private let operation: CKModifyRecordsOperation
+    
+    private(set) var savedRecords: [CKRecord]?
+
+    private(set) var deletedRecordIDs: [CKRecordID]?
+
+    var savePolicy: CKRecordSavePolicy {
+        set {
+            operation.savePolicy = newValue
+        }
+        get {
+            return operation.savePolicy
+        }
+    }
+
+    // MARK: - Initialization
+
+    init(recordsToSave: [CKRecord]?, recordIDsToDelete: [CKRecordID]?, database: CKDatabase) {
+        self.operation = CKModifyRecordsOperation(recordsToSave: recordsToSave, recordIDsToDelete: recordIDsToDelete)
+        super.init(database: database)
+    }
+
+    // MARK: - Execution
+
+    override func execute() {
+        operation.modifyRecordsCompletionBlock = { savedRecords, deletedRecordIDs, error in
+            self.savedRecords = savedRecords
+            self.deletedRecordIDs = deletedRecordIDs
+            self.finish(error)
+        }
+        database.addOperation(operation)
+    }
+}
