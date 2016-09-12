fca2619f84e7697ffe5b3a335f417d11699f72ee
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-12 14:25:31 +0200

    Create Location

---
+++ ESTPEvents/Classes/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 11/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+    let title: String
+    let subtitle: String
+}
