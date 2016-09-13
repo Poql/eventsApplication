f6ed1e5811cc6d07514c6ee9413cbf90a9507c90
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-13 09:08:48 +0200

    Create rows mapper in EventDetailRow

--- ESTPEvents/Classes/EventDetailRow.swift
+++ ESTPEvents/Classes/EventDetailRow.swift
@@ -14,3 +14,29 @@ enum EventDetailRow: Int {
     case url
     case location
 }
+
+struct EventDetailRowMapper {
+    static func row(atIndexPath indexPath: NSIndexPath, forEvent event: Event?) -> EventDetailRow? {
+        let hasURL = event?.link != nil
+        switch indexPath.row {
+        case 0:
+            return .preview
+        case 1:
+            return .description
+        case 2:
+            return hasURL ? .url : .location
+        case 3:
+            return .location
+        default:
+            return nil
+        }
+    }
+
+    static func numberOfRows(forEvent event: Event?) -> Int {
+        guard let event = event else { return 0 }
+        var count = EventDetailRow.count
+        count -= event.link != nil ? 0 : 1
+        count -= event.location != nil ? 0 : 1
+        return count
+    }
+}
