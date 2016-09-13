4556e966be54c132b85beff962eeec2d5d7e6eba
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-13 09:06:52 +0200

    Create EventDetailRow

---
+++ ESTPEvents/Classes/EventDetailRow.swift
@@ -0,0 +1,16 @@
+//
+//  EventDetailRow.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 12/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+enum EventDetailRow: Int {
+    case preview = 0
+    case description
+    case url
+    case location
+}
