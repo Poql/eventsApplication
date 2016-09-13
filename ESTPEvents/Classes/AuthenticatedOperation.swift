49543e08aaeb73634dc92321a7ab6b4587e6c03a
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-13 14:37:20 +0200

    Create AuthenticatedOperation

---
+++ ESTPEvents/Classes/AuthenticatedOperation.swift
@@ -0,0 +1,15 @@
+//
+//  AuthenticatedOperation.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 13/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import Operations
+import CloudKit
+
+protocol AuthenticatedOperation: class {
+    var userID: CKRecordID? { get set }
+}
