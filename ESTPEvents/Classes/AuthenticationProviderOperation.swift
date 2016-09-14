2cb6c7cbf7e159a2b790c570a1145e51b2a50518
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-14 12:06:01 +0200

    Create AuthentificationProviserOperation

---
+++ ESTPEvents/Classes/AuthenticationProviderOperation.swift
@@ -0,0 +1,33 @@
+//
+//  AuthenticationProviderOperation.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 14/09/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import Operations
+import CloudKit
+
+class AuthenticationProviderOperation: GroupOperation {
+    private let fetchUserOperation = FetchUserOperation(container: CKContainer(identifier: "iCloud.com.gzanella.ESTPEvents"))
+
+    private var authenticatedOperations: [AuthenticatedOperation] {
+        return operations.flatMap { $0 as? AuthenticatedOperation }
+    }
+
+    init() {
+        super.init(operations: [fetchUserOperation])
+    }
+
+    override func operationQueue(queue: OperationQueue, willFinishOperation operation: NSOperation, withErrors errors: [ErrorType]) {
+        guard fetchUserOperation == operation else { return }
+        authenticatedOperations.forEach { $0.injectUserID(fetchUserOperation.userID) }
+    }
+
+    func addAuthenticatedOperation<O: Operation where O: AuthenticatedOperation>(operation: O) {
+        (operation as Operation).addDependency(fetchUserOperation)
+        addOperation(operation as Operation)
+    }
+}
