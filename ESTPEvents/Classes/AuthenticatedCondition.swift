c5e43e34b6350429bcd27ea8d8eb9fa5806165e9
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-14 12:05:26 +0200

    Remove user ID injection from AuthenticatedCondition

--- ESTPEvents/Classes/AuthenticatedCondition.swift
+++ ESTPEvents/Classes/AuthenticatedCondition.swift
@@ -11,22 +11,11 @@ import Operations
 import CloudKit
 
 class AuthenticatedCondition: Condition {
-
-    private var userID: CKRecordID?
-
-    override init() {
-        super.init()
-        let operation = FetchUserOperation(container: CKContainer(identifier: "iCloud.com.gzanella.ESTPEvents"))
-        operation.addWillFinishBlock { self.userID = operation.userID }
-        addDependency(operation)
-    }
-
     override func evaluate(operation: Operation, completion: CompletionBlockType) {
         guard let operation = operation as? AuthenticatedOperation else {
             completion(.Ignored)
             return
         }
-        operation.userID = userID
-        completion(userID == nil ? .Failed(OperationError.notAuthenticated) : .Satisfied)
+        completion(operation.userID == nil ? .Failed(OperationError.notAuthenticated) : .Satisfied)
     }
 }
