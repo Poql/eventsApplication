eb27fc1568e5a599d9c8d5cccebb4a8d1bd007a1
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-21 17:20:52 +0200

    Add banner in ShareViewController

--- ESTPEvents/Classes/SharedViewController.swift
+++ ESTPEvents/Classes/SharedViewController.swift
@@ -23,15 +23,51 @@ class SharedViewController: UIViewController, UserStatusUpdateListener {
         return AppDelegate.shared.presenterFactory
     }
 
+    private var identifiers: [Int] = []
+    private var currentInfo: [Int : Info] = [:]
+
+    private let bannerContainer = BannerContainerView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBanner()
         userStatusDidUpdate(presenterFactory.applicationPresenter.currentUserStatus)
         presenterFactory.applicationPresenter.registerForUserStatusUpdate(self)
+    }
+
+    // MARK: - Private
+
+    func setupBanner() {
+        edgesForExtendedLayout = UIRectEdge.None
+        view.addSubview(bannerContainer)
+        bannerContainer.translatesAutoresizingMaskIntoConstraints = false
+        bannerContainer.topAnchor.constraintEqualToAnchor(topLayoutGuide.topAnchor).active = true
+        bannerContainer.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
+        bannerContainer.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor).active = true
     }
 
     // MARK: - UserStatusUpdateListener
 
     func userStatusDidUpdate(userStatus: UserStatus) {
+    }
+
+    // MARK: - Public
+
+    func showBanner(with info: Info, animated: Bool = true) {
+        identifiers.append(info.identifier)
+        currentInfo[info.identifier] = info
+        bannerContainer.showBanner(with: info.description, color: info.color, animated: animated)
+    }
+
+    func dismissBannerInfo(info: Info) {
+        guard let i = identifiers.indexOf(info.identifier) else { return }
+        identifiers.removeAtIndex(i)
+        currentInfo.removeValueForKey(i)
+        if let identifier = identifiers.last, let info = currentInfo[identifier] {
+            showBanner(with: info)
+            return
+        }
+        bannerContainer.hideBanner()
     }
 }
 
