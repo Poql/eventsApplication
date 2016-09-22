82a24c91930bb3e431ac215480bebcdaef17a493
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-09-22 09:23:14 +0200

    Add hiding empty event view when loading in EventViewController

--- ESTPEvents/Classes/EventViewController.swift
+++ ESTPEvents/Classes/EventViewController.swift
@@ -166,6 +166,7 @@ class EventViewController: SharedViewController, EventPresenterClient, UITableVi
     }
 
     func presenterWantsToShowLoading() {
+        emptyView.hidden = true
     }
     
     func presenterWantsToDismissLoading() {
