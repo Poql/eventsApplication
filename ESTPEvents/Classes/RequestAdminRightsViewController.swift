507f402e62bc3a78dfa34659f2dd80c5e33b5c7e
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-10-23 11:05:12 +0200

    Create RequestAdminRightsViewController

---
+++ ESTPEvents/Classes/RequestAdminRightsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  RequestAdminRightsViewController.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 22/10/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+class RequestAdminRightsViewController: SharedViewController, MainPresenterClient {
+
+    @IBOutlet var cancelButton: UIButton!
+
+    @IBOutlet var titleLabel: UILabel!
+
+    @IBOutlet var subtitleLabel: UILabel!
+
+    @IBOutlet var requestButton: UIButton!
+
+    @IBOutlet var detailsLabel: UILabel!
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+
+    @IBOutlet var bottomStackView: UIStackView!
+
+    private lazy var mainPresenter: MainPresenter = {
+        let presenter = self.presenterFactory.mainPresenter
+        self.presenterFactory.addClient(self)
+        return presenter
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupController()
+    }
+
+    // MARK: - Private
+
+    private func setupController() {
+        titleLabel.text = String(key: "request_admin_rights_title")
+        subtitleLabel.text = String(key: "request_admin_rights_subtitle")
+        detailsLabel.text = String(key: "request_admin_rights_details")
+        requestButton.setTitle(String(key: "request_admin_rights_button_title"), forState: .Normal)
+        cancelButton.setTitle(String(key: "cancel_button_title"), forState: .Normal)
+        cancelButton.addTarget(self, action: #selector(cancelAction(_:)), forControlEvents: .TouchUpInside)
+        requestButton.addTarget(self, action: #selector(requestButtonAction(_:)), forControlEvents: .TouchUpInside)
+    }
+
+    // MARK: - Action
+
+    @objc private func requestButtonAction(sender: UIButton) {
+        mainPresenter.requestToBecomeAdmin()
+    }
+
+    @objc private func cancelAction(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // MARK: - MainPresenterClient
+
+    func presenterWantsToShowError(error: ApplicationError) {
+        bottomStackView.hidden = false
+        activityIndicator.stopAnimating()
+        showAlert(withMessage: error.description, title: String(key: "error_title"))
+    }
+
+    func presenterDidRequestToBecomeAdminSuccessfullly() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func presenterWantsToShowLoading() {
+        bottomStackView.hidden = true
+        activityIndicator.startAnimating()
+    }
+}
