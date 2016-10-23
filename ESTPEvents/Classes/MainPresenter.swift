1fa03f667622e6e9d28483326d03a670bec426d6
Gaétan Zanella <gaetanzanella@macbook-pop.home>
2016-10-23 10:59:30 +0200

    Create MainPresenter

---
+++ ESTPEvents/Classes/MainPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MainPresenter.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 19/10/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+protocol MainPresenter {
+    func requestToBecomeAdmin()
+}
