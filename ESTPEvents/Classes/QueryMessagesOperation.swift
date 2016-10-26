5af3d4879f7cfb194b68ceda1e794dc0accbf3dd
Gaétan Zanella <gaetan.zanella@gmail.com>
2016-10-26 20:08:28 +0200

    Create QueryMessagesOperation

---
+++ ESTPEvents/Classes/QueryMessagesOperation.swift
@@ -0,0 +1,23 @@
+//
+//  QueryMessagesOperation.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 23/10/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class QueryMessagesOperation: QueryOperation, QueryMessagesOperationPrototype {
+    var messages: [Message] {
+        return fetchedRecords.map { Message(record: $0) }
+    }
+
+    init() {
+        let predicate = NSPredicate(format: "TRUEPREDICATE")
+        let query = CKQuery(record: Message.self, predicate: predicate)
+        super.init(query: query)
+    }
+}
+
