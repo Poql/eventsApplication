5ff89036eefb622915c46517f05414ae63de6abc
Gaétan Zanella <gaetan.zanella@gmail.com>
2016-11-11 10:49:17 +0100

    Create UIFont category

---
+++ ESTPEvents/Classes/UIFont.swift
@@ -0,0 +1,83 @@
+//
+//  UIFont.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 11/11/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    // MARK: - Main
+
+    static func regularMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Regular", size: size)!
+    }
+    static func regularItalicMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Italic", size: size)!
+    }
+    static func blackMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Black", size: size)!
+    }
+    static func blackItalicMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-BlackItalic", size: size)!
+    }
+    static func boldMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Bold", size: size)!
+    }
+    static func boldItalicMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-BoldItalic", size: size)!
+    }
+    static func lightMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Light", size: size)!
+    }
+    static func lightItalicMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-LightItalic", size: size)!
+    }
+    static func mediumMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Medium", size: size)!
+    }
+    static func mediumItalicMainFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-MediumItalic", size: size)!
+    }
+
+    // MARK: - Header
+
+    static func regularHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-Regular", size: size)!
+    }
+    static func regularItalicHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-RegularItalic", size: size)!
+    }
+    static func heavyHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-Heavy", size: size)!
+    }
+    static func heavyItalicHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-HeavyItalic", size: size)!
+    }
+    static func boldHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-Bold", size: size)!
+    }
+    static func boldItalicHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-BoldItalic", size: size)!
+    }
+    static func lightHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-Light", size: size)!
+    }
+    static func lightItalicHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-LightItalic", size: size)!
+    }
+    static func mediumHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-Medium", size: size)!
+    }
+    static func mediumItalicHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-MediumItalic", size: size)!
+    }
+    static func semiboldHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-Semibold", size: size)!
+    }
+    static func semiboldItalicHeaderFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "SFMono-SemiboldItalic", size: size)!
+    }
+}
