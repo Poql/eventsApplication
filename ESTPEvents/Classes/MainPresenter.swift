3bcf3830926746a5332691a73d97949376217220
Gaétan Zanella <gaetan.zanella@gmail.com>
2016-11-15 22:12:15 +0100

    Create MainPresenter & implementation

---
+++ ESTPEvents/Classes/MainPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  MainPresenter.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 13/11/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+protocol MainPresenter {
+    var currentStateFile: ApplicationStateFile? { get }
+    var applicationIsOpened: Bool { get }
+    func updateCurrentStateFile()
+}
+
+protocol MainPresenterClient: class {
+    func presenterDidDiscoverInvalidApplicationVersion()
+    func presenterWantsToUpdateTintColor(hex: String)
+}
---
+++ ESTPEvents/Classes/MainPresenterImplementation.swift
@@ -0,0 +1,34 @@
+//
+//  MainPresenterImplementation.swift
+//  ESTPEvents
+//
+//  Created by Gaétan Zanella on 13/11/2016.
+//  Copyright © 2016 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+import Operations
+
+
+class MainPresenterImplementation<Repository: UserStatusRepository>: ConfigurationPresenter<Repository>, MainPresenter {
+
+    weak var client: MainPresenterClient?
+
+    override init(repository: Repository) {
+        super.init(repository: repository)
+    }
+
+    // MARK: - ConfigurationPresenter
+
+    func updateCurrentStateFile() {
+        updateCurrentStateFile(showLoading: true)
+    }
+
+    override func presenterDidUpdateTintColor(colorHex: String) {
+        client?.presenterWantsToUpdateTintColor(colorHex)
+    }
+
+    override func presenterDidDiscoverInvalidApplicationVersion() {
+        client?.presenterDidDiscoverInvalidApplicationVersion()
+    }
+}                                                                                                                                                                                         
